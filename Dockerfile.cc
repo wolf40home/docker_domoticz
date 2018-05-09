d1b194ed5c7e4c655f60170ab7266f8c4250b065
Cedric Gatay <c.gatay@code-troopers.com>
2018-05-09 20:46:36 +0200

    feat(armBuild): add python dependency for runtime
    Missing python dependency

--- Dockerfile.cc
+++ Dockerfile.cc
@@ -94,7 +94,7 @@ RUN ["cross-build-start"]
 RUN \
   apt-get update && \
   apt-get install -y cmake apt-utils build-essential && \
-  apt-get install -y libboost-dev libboost-thread-dev libboost-system-dev libsqlite3-dev subversion curl libcurl4-openssl-dev libusb-dev zlib1g-dev && \
+  apt-get install -y libboost-dev libboost-thread-dev libboost-system-dev libsqlite3-dev subversion curl libcurl4-openssl-dev libusb-dev zlib1g-dev libpython3-dev && \
   apt-get install -y iputils-ping && \
   apt-get clean && \
   apt-get autoclean && \
